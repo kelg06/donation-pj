9e7bf97f699258c87a6f7a69d48d7bb0ce4f15e2
kelg06 <mgladney@basecampcodingacademy.org>
2025-01-06 13:00:03 -0600

    coperrrr

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from app.views import *
 urlpatterns = [
     path('signup', signup, name='register'),
     path('login', loginPage ,name='login'),
+    path('logout', logoutUser ,name='logout'),
     path("", home, name="home"),
     path("about/", about, name="about"),
     path("contact/", contact, name="contact"),
