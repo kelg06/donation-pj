bc402d1408f2aad33b58734cdd748542458f8111
kelg06 <mgladney@basecampcodingacademy.org>
2025-01-06 12:41:00 -0600

    coper

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from app.views import *
 
 urlpatterns = [
+    path('signup', signup, name='register'),
+    path('login', loginPage ,name='login'),
     path("", home, name="home"),
     path("about/", about, name="about"),
     path("contact/", contact, name="contact"),
